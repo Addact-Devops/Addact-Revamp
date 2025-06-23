e53bee6bf1fe8c7da3c65c5b6cb7ddce86860415
Yash <yash@addact.net>
2025-06-23 14:20:58 +0530

    HomePage-FE

--- addact/tailwind.config.js
+++ addact/tailwind.config.js
@@ -1,10 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // include your src folder
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
+    content: [
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        colors: {
+                grayCustom: "#2e2e2e",
+            },
+        extend: {
+            colors: {
+                grayCustom: "#2e2e2e",
+            },
+            animation: {
+                marquee: "scroll-marquee 20s linear infinite",
+                fadeWord: "fadeWord 1.3s ease forwards",
+            },
+            keyframes: {
+                "scroll-marquee": {
+                    "0%": { transform: "translateX(0%)" },
+                    "100%": { transform: "translateX(-50%)" },
+                },
+                fadeWord: {
+                    from: { opacity: "0.5" },
+                    to: { opacity: "1" },
+                },
+            },
+        },
+    },
+    plugins: [require("@tailwindcss/typography")],
 };
