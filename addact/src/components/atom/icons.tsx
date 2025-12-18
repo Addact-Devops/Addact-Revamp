7844825701515a7cba7af4029b3197db720d50f8
devam-addact <devam@addact.net>
2025-12-18 19:55:46 +0530

    feat: Implement breadcrumbs component and enhance BlogDetailBanner with breadcrumb navigation

--- addact/src/components/atom/icons.tsx
+++ addact/src/components/atom/icons.tsx
@@ -1,47 +1,84 @@
 import React from "react";
 
 export const RightArrowIcon: React.FC = () => {
-    return (
-        <svg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'>
-            <path
-                d='M18.0383 7.4126L25.6258 15.0001L18.0383 22.5876'
-                stroke='#3C4CFF'
-                strokeWidth='2'
-                strokeMiterlimit='10'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-            />
-            <path
-                d='M4.375 15L25.4125 15'
-                stroke='#3C4CFF'
-                strokeWidth='2'
-                strokeMiterlimit='10'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-            />
-        </svg>
-    );
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18.0383 7.4126L25.6258 15.0001L18.0383 22.5876"
+        stroke="#3C4CFF"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4.375 15L25.4125 15"
+        stroke="#3C4CFF"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
 };
 
-export const RightArrowUpIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
-    return (
-        <svg width='44' height='44' viewBox='0 0 44 44' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-            <path
-                d='M18.7832 14.4863L29.5135 14.4863L29.5135 25.2167'
-                stroke='white'
-                strokeWidth='2'
-                strokeMiterlimit='10'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-            />
-            <path
-                d='M14.4883 29.5127L29.364 14.6369'
-                stroke='white'
-                strokeWidth='2'
-                strokeMiterlimit='10'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-            />
-        </svg>
-    );
+export const RightArrowUpIcon: React.FC<React.SVGProps<SVGSVGElement>> = (
+  props
+) => {
+  return (
+    <svg
+      width="44"
+      height="44"
+      viewBox="0 0 44 44"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M18.7832 14.4863L29.5135 14.4863L29.5135 25.2167"
+        stroke="white"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.4883 29.5127L29.364 14.6369"
+        stroke="white"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export const CaretRightIcon: React.FC<React.SVGProps<SVGSVGElement>> = (
+  props
+) => {
+  return (
+    <svg
+      width="20px"
+      height="40px"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M6.18194 4.18185C6.35767 4.00611 6.6426 4.00611 6.81833 4.18185L9.81833 7.18185C9.90272 7.26624 9.95013 7.3807 9.95013 7.50005C9.95013 7.6194 9.90272 7.73386 9.81833 7.81825L6.81833 10.8182C6.6426 10.994 6.35767 10.994 6.18194 10.8182C6.0062 10.6425 6.0062 10.3576 6.18194 10.1819L8.86374 7.50005L6.18194 4.81825C6.0062 4.64251 6.0062 4.35759 6.18194 4.18185Z"
+        fill="white"
+      />
+    </svg>
+  );
 };
