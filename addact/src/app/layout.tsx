8eda8c5cfe19d50d46f92d8580738275f973bd11
devam-addact <devam@addact.net>
2026-01-09 17:24:25 +0530

    feat: added markup schema script

--- addact/src/app/layout.tsx
+++ addact/src/app/layout.tsx
@@ -100,6 +100,78 @@ export default async function RootLayout({
                         `,
           }}
         />
+
+        {/*  Markup schema */}
+        <Script
+          id="website-schema"
+          type="application/ld+json"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              url: "https://www.addact.net/",
+              name: "Addact Technologies",
+              "@type": "WebSite",
+              "@context": "https://schema.org/",
+              potentialAction: {
+                "@type": "SearchAction",
+                target: "{search_term_string}",
+                "query-input": "required name=search_term_string",
+              },
+            }),
+          }}
+        />
+
+        <Script
+          id="faqpage-schema"
+          type="application/ld+json"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "FAQPage",
+              mainEntity: {
+                "@type": "Question",
+                name: "What CMS platforms does Addact specialize in?",
+                acceptedAnswer: {
+                  "@type": "Answer",
+                  text: "Addact provides end-to-end development, consulting, and support services across leading CMS platforms, including Sitecore, Umbraco, Kentico, Strapi, Contentful, and Contentstack.",
+                },
+              },
+            }),
+          }}
+        />
+
+        <Script
+          id="organization-schema"
+          type="application/ld+json"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              name: "Addact Technologies",
+              alternateName: "Addact",
+              url: "https://www.addact.net/",
+              logo: "https://d3l7d9gtq0bnch.cloudfront.net/Logo_1_ffdf03e2d1.png",
+              contactPoint: {
+                "@type": "ContactPoint",
+                telephone: "94272 37737",
+                contactType: "technical support",
+                areaServed: ["US", "IN"],
+                availableLanguage: "en",
+              },
+              sameAs: [
+                "https://www.facebook.com/addacttech/",
+                "https://x.com/AddactTech",
+                "https://www.instagram.com/addacttechnologies/",
+                "https://www.youtube.com/@addact3283",
+                "https://www.linkedin.com/company/addact-technologies/",
+              ],
+            }),
+          }}
+        />
+
+        {/*  End Markup schema */}
       </body>
     </html>
   );
