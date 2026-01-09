32d376927f60037f9082f313da26b5b209ccb0ea
devam-addact <devam@addact.net>
2026-01-09 11:09:59 +0530

    update nextconfig for tawk.to

--- addact/src/components/organisms/TwakTo.tsx
+++ addact/src/components/organisms/TwakTo.tsx
@@ -1,16 +1,18 @@
 "use client";
 
+import Head from "next/head";
 import Script from "next/script";
 import { Fragment } from "react";
 
 export default function TawkTo() {
   return (
     <Fragment>
-      <Script
-        id="tawk-to"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+      <Head>
+        <Script
+          id="tawk-to"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
             var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
             (function () {
               var s1 = document.createElement("script"),
@@ -22,8 +24,9 @@ export default function TawkTo() {
               s0.parentNode.insertBefore(s1, s0);
             })();
           `,
-        }}
-      />
+          }}
+        />
+      </Head>
     </Fragment>
   );
 }
