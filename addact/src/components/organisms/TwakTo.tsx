5d04c6f0d6883789f22ec32a531006760d534fde
devam-addact <devam@addact.net>
2026-01-09 11:13:04 +0530

    remove: head from tawk.to component

--- addact/src/components/organisms/TwakTo.tsx
+++ addact/src/components/organisms/TwakTo.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import Head from "next/head";
 import Script from "next/script";
 import { Fragment } from "react";
 
 export default function TawkTo() {
   return (
     <Fragment>
-      <Head>
-        <Script
-          id="tawk-to"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
+      <Script
+        id="tawk-to"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
             var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
             (function () {
               var s1 = document.createElement("script"),
@@ -24,9 +22,8 @@ export default function TawkTo() {
               s0.parentNode.insertBefore(s1, s0);
             })();
           `,
-          }}
-        />
-      </Head>
+        }}
+      />
     </Fragment>
   );
 }
