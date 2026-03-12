6bb469a62b139a752213a57aa13a12c30eb490fc
Keyur Nayi <149389057+KeyurNayi@users.noreply.github.com>
2026-03-12 17:50:06 +0530

    Changes Update

---
+++ addact-cms/src/api/cms-detail/controllers/cms-detail.ts
@@ -0,0 +1,7 @@
+/**
+ * cms-detail controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::cms-detail.cms-detail');
---
+++ addact-cms/src/api/sitecore-detail/controllers/sitecore-detail.ts
@@ -0,0 +1,7 @@
+/**
+ * sitecore-detail controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::sitecore-detail.sitecore-detail');
