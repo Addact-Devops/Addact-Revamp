356a304a27b5cd3927d80fbee4ac32650edc65ac
devam <devam@addact.net>
2026-03-14 02:41:31 +0530

    feat: added digital marketing with dynamic slug queries

---
+++ addact/src/app/digital-marketing/[slug]/SiteDetailClient.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { useEffect, useState } from "react";
+// import dynamic from "next/dynamic";
+import { useParams } from "next/navigation";
+
+import HeroBanner from "@/components/organisms/HeroBanner";
+// import OurProcess from "@/components/organisms/OurProcess";
+import ClientTestimonials from "@/components/organisms/ClientTestimonials";
+import OurInsights from "@/components/organisms/OurInsights";
+import FAQ from "@/components/organisms/FAQ";
+// import ServiceCtaBanner2 from "@/components/molecules/ServiceCtaBanner2";
+import HowEngagementProcessWorks from "@/components/organisms/HowEngagementProcessWorks";
+import CtaBanner from "@/components/molecules/CtaBanner";
+import IndustryMarqueeCards from "@/components/organisms/IndustryMarqueeCards";
+import {
+  DigitalMarketingService,
+  getDigitalMarketingSlug,
+} from "@/graphql/queries/getDigitalMarketingSlug";
+import WhoWeAre from "@/components/organisms/WhoWeAre";
+import DetailPageServices from "@/components/organisms/DetailPageServices";
+import WhyWorkWithUs from "@/components/organisms/WhyWorkWithUs";
+import OurTechStack from "@/components/organisms/OurTechStack";
+
+// const IndustriesWeServe = dynamic(
+//   () => import("@/components/organisms/IndustriesWeServe"),
+//   { ssr: false },
+// );
+
+const SiteDetailClient = ({ data }: { data: DigitalMarketingService }) => {
+  const [pageData, setPageData] = useState<DigitalMarketingService | null>(
+    data,
+  );
+  const [loading, setLoading] = useState(false); // set false, we already have data
+
+  const params = useParams();
+  const slug =
+    typeof params?.slug === "string"
+      ? params.slug
+      : Array.isArray(params?.slug)
+        ? params.slug[0]
+        : "";
+
+  useEffect(() => {
+    if (!pageData && slug) {
+      setLoading(true);
+      getDigitalMarketingSlug(slug)
+        .then((res) => {
+          setPageData(res);
+        })
+        .catch((err) => {
+          console.error("Error fetching service detail:", err);
+        })
+        .finally(() => setLoading(false));
+    }
+  }, [slug, pageData]);
+
+  if (loading) {
+    return <div className="text-white p-8">Loading...</div>;
+  }
+
+  if (!pageData) {
+    return <div className="text-white p-8">Page Not Found</div>;
+  }
+
+  const bannerData = pageData.Banner?.Banner?.[0];
+
+  return (
+    <main className="bg-dark">
+      <HeroBanner
+        title={bannerData?.BannerTitle ?? ""}
+        description={
+          bannerData?.BannerDescription?.replace(/^<p>|<\/p>$/g, "") ?? ""
+        }
+        button={{
+          label: bannerData?.BannerLink?.label ?? "",
+          url: bannerData?.BannerLink?.href ?? "",
+        }}
+        backgroundImageUrl={bannerData?.BannerImage?.url ?? ""}
+      />
+      <WhoWeAre />
+      <DetailPageServices title="Our Services Carousel Test" isCaraousl />
+      <DetailPageServices title="Our Services Grid Test" />
+      {data?.why_addact && <WhyWorkWithUs data={data.why_addact} />}
+      {/* {data?.cta2 && <CtaBanner2 data={data?.cta2} />} */}
+      {/* <OurProcess data={data?.our_process} /> */}
+      <OurTechStack data={data?.techStack} />
+      <HowEngagementProcessWorks data={data?.ourprocess} />
+      <IndustryMarqueeCards />
+      <ClientTestimonials />
+      {data?.faq && <FAQ data={data?.faq} />}
+      <OurInsights />
+      {data?.cta && <CtaBanner data={data?.cta} />}
+    </main>
+  );
+};
+
+export default SiteDetailClient;
---
+++ addact/src/graphql/queries/getDigitalMarketing.ts
@@ -0,0 +1,443 @@
+import { gql } from "graphql-request";
+import client from "../client";
+import { Heading, Image, Link } from "./getHomePage";
+
+const digitalMarketingQuery = gql`
+  query DigitalMarketingLanding {
+    digitalMarketingService {
+      SEO {
+        metaTitle
+        metaDescription
+        ogTitle
+        ogDescription
+        ogImage {
+          url
+        }
+        metaRobots
+        twitterCardTitle
+        canonicalURL
+        structuredData
+        languageTag
+      }
+      Banner {
+        Banner {
+          ... on ComponentBannerBanner {
+            BannerTitle
+            BannerDescription
+            BannerLogo {
+              alternativeText
+              height
+              url
+              width
+            }
+            BannerImage {
+              alternativeText
+              height
+              url
+              width
+            }
+            isTextAlignCenter
+            isVideo
+            show_searchbox
+            videoLink
+            BannerLink {
+              id
+              href
+              label
+              target
+              isExternal
+              SubDisc
+              Icon {
+                alternativeText
+                height
+                url
+                width
+              }
+            }
+          }
+        }
+      }
+      cta {
+        CTADescription
+        pageReference
+        CTAImage {
+          ... on ComponentSharedImage {
+            Image {
+              alternativeText
+              height
+              name
+              url
+              width
+            }
+          }
+        }
+        CTALink {
+          ... on ComponentSharedLink {
+            href
+            id
+            isExternal
+            label
+            target
+          }
+        }
+        Title {
+          ... on ComponentHeadingsH6 {
+            id
+            h6
+          }
+          ... on ComponentHeadingsH5 {
+            id
+            h5
+          }
+          ... on ComponentHeadingsH4 {
+            id
+            h5
+          }
+          ... on ComponentHeadingsH3 {
+            id
+            h3
+          }
+          ... on ComponentHeadingsH2 {
+            id
+            h2
+          }
+          ... on ComponentHeadingsH1 {
+            id
+            h1
+          }
+        }
+      }
+
+      whyaddact {
+        Title {
+          ... on ComponentHeadingsH1 {
+            id
+            h1
+          }
+          ... on ComponentHeadingsH2 {
+            id
+            h2
+          }
+          ... on ComponentHeadingsH3 {
+            id
+            h3
+          }
+          ... on ComponentHeadingsH4 {
+            id
+            h5
+          }
+          ... on ComponentHeadingsH5 {
+            id
+            h5
+          }
+          ... on ComponentHeadingsH6 {
+            id
+            h6
+          }
+        }
+        pageReference
+        GlobalCard {
+          ... on ComponentBaseTemplatePromo {
+            id
+            Title
+            Description
+            Image {
+              alternativeText
+              height
+              name
+              url
+              width
+            }
+            Link {
+              id
+              href
+              label
+              target
+              isExternal
+            }
+          }
+        }
+      }
+      faq {
+        Title
+        FAQ {
+          Description
+          Title
+          id
+        }
+      }
+
+      techStack {
+        title
+        description
+        tab {
+          category {
+            categoryTitle
+          }
+          tabContent {
+            title
+            logo {
+              alternativeText
+              height
+              url
+              width
+            }
+          }
+        }
+      }
+
+      ourService {
+        ... on ComponentHomeDigitalMarketingListing {
+          id
+          serviceTitle
+          serviceList {
+            listingContext {
+              id
+              title
+              description
+              image {
+                alternativeText
+                url
+                width
+                height
+              }
+              link {
+                id
+                href
+                label
+                target
+                isExternal
+                SubDisc
+                Icon {
+                  alternativeText
+                  height
+                  url
+                  width
+                }
+              }
+            }
+          }
+
+          isCarousel
+        }
+        ... on ComponentHomeServiceList {
+          id
+          serviceTitle
+          serviceList {
+            listingContext {
+              id
+              title
+              description
+              image {
+                alternativeText
+                url
+                width
+                height
+              }
+              link {
+                id
+                href
+                label
+                target
+                isExternal
+                SubDisc
+                Icon {
+                  alternativeText
+                  height
+                  url
+                  width
+                }
+              }
+            }
+          }
+          isCarousel
+        }
+      }
+
+      ourprocess {
+        Title {
+          ... on ComponentHeadingsH1 {
+            id
+            h1
+          }
+          ... on ComponentHeadingsH2 {
+            id
+            h2
+          }
+          ... on ComponentHeadingsH3 {
+            id
+            h3
+          }
+          ... on ComponentHeadingsH4 {
+            id
+            h5
+          }
+          ... on ComponentHeadingsH5 {
+            id
+            h5
+          }
+          ... on ComponentHeadingsH6 {
+            id
+            h6
+          }
+        }
+        ProcessData {
+          ... on ComponentBaseTemplateTitleWithDescription {
+            id
+            Title
+            Description
+          }
+        }
+      }
+    }
+  }
+`;
+
+export interface DigitalMarketingResponse {
+  digitalMarketingService: DigitalMarketingService;
+}
+
+export interface DigitalMarketingService {
+  SEO: SEO | null;
+  Banner: BannerSection;
+  cta: CTA | null;
+  whyaddact: Whyaddact | null;
+  faq: FAQ;
+  techStack: TechStack;
+  ourService?: OurServiceList;
+  ourprocess: OurProcess;
+}
+
+export interface OurServiceList {
+  isCarousel: boolean;
+  serviceTitle: string;
+  serviceList: ServiceListItem[];
+}
+
+export interface ServiceListItem {
+  listingContext: {
+    title: string;
+    description: string;
+    image: Image | null;
+    link: {
+      id: string;
+      href: string;
+      label: string;
+      target: string;
+      isExternal: boolean;
+      SubDisc: string | null;
+      Icon: Image | null;
+    } | null;
+  };
+}
+
+export interface SEO {
+  metaTitle: string;
+  metaDescription: string;
+  ogTitle: string;
+  ogDescription: string;
+  ogImage: {
+    url: string;
+  } | null;
+  metaRobots: string;
+  twitterCardTitle: string;
+  canonicalURL: string;
+  structuredData: string | null;
+  languageTag: string;
+}
+
+export interface BannerSection {
+  Banner: BannerItem[];
+}
+
+export interface BannerItem {
+  BannerTitle: string;
+  BannerDescription: string;
+  BannerLogo: Image | null;
+  BannerImage: Image | null;
+  isTextAlignCenter: boolean | null;
+  isVideo: boolean | null;
+  show_searchbox: boolean;
+  videoLink: string | null;
+  BannerLink: BannerLink;
+}
+
+export interface BannerLink {
+  id: string;
+  href: string;
+  label: string;
+  target: string;
+  isExternal: boolean;
+  SubDisc: string | null;
+  Icon: Image | null;
+}
+
+export interface CTA {
+  CTADescription: string;
+  pageReference: string;
+  CTAImage: {
+    Image: Image;
+  }[];
+  CTALink: Link[];
+  Title: Heading[];
+}
+
+export interface Whyaddact {
+  Title: Heading[];
+  pageReference?: string;
+  GlobalCard: GlobalCard2[];
+}
+export interface GlobalCard2 {
+  id?: string;
+  Title: string;
+  Description: string;
+  Image: Image;
+  Link?: Link | null;
+}
+
+export interface FAQ {
+  Title: string;
+  FAQ: {
+    id: string;
+    Title: string;
+    Description: string;
+  }[];
+}
+
+export interface TechStack {
+  title: string;
+  description: string;
+  tab: Tab[];
+}
+
+export interface Tab {
+  category: {
+    categoryTitle: string;
+  };
+  tabContent: TabContent[];
+}
+
+export interface TabContent {
+  title: string;
+  logo: Image | null;
+}
+
+export interface OurProcess {
+  Title: Heading[];
+  ProcessData: ProcessDataItem[];
+}
+
+export interface ProcessDataItem {
+  id: string;
+  Title: string;
+  Description: string;
+}
+
+// Fetch function
+export async function getDigitalMarketing(): Promise<DigitalMarketingService> {
+  const data = await client.request<DigitalMarketingResponse>(
+    digitalMarketingQuery,
+  );
+
+  return data.digitalMarketingService;
+}
