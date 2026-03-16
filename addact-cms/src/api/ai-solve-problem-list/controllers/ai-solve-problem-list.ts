837ade76128b7a9a0d64cb6fb574f201aa120332
Keyur Nayi <149389057+KeyurNayi@users.noreply.github.com>
2026-03-16 18:26:39 +0530

    Changes
    Changes

---
+++ addact-cms/src/api/ai-solve-problem-list/controllers/ai-solve-problem-list.ts
@@ -0,0 +1,7 @@
+/**
+ * ai-solve-problem-list controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::ai-solve-problem-list.ai-solve-problem-list');
