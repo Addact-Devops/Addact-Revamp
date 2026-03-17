fcb108839654a1446b3c61a428ae3530707f1d0f
devam <devam@addact.net>
2026-03-17 16:35:50 +0530

    feat: added UIUX why important component for uiux services

--- addact/src/components/organisms/UIUXPageFlow.tsx
+++ addact/src/components/organisms/UIUXPageFlow.tsx
@@ -1,12 +1,14 @@
 import type { PageData } from "@/app/development-design/[...slug]/SiteDetailClient";
 import React from "react";
 import UIUXHeroBanner from "./UIUXHeroBanner";
+import UIUXWhyImportant from "./UIUXWhyImportant";
 
 const UIUXPageFlow = ({ data }: { data: PageData }) => {
   const bannerData = data?.Banner?.Banner?.[0] ?? null;
   return (
     <>
       <UIUXHeroBanner data={bannerData} />
+      <UIUXWhyImportant />
     </>
   );
 };
